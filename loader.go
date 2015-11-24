fc37711fcb5733461225806c1cd53eb16cba3665
Garret Raziel <boloomka@gmail.com>
2015-11-24 22:28:02 +0100

    return input length

--- loader.go
+++ loader.go
@@ -34,11 +34,11 @@ func ReadLabels(path string) ([]float64, int, error) {
     return items, len(distinct), nil
 }
 
-// ReadImages parses input image file and returns list of MNIST images
-func ReadImages(path string) ([][]float64, error) {
+// ReadImages parses input image file and returns list of MNIST images and length of one input value
+func ReadImages(path string) ([][]float64, int, error) {
     f, err := os.Open(path)
     if err != nil {
-        return nil, err
+        return nil, 0, err
     }
 
     var magic int32
@@ -46,27 +46,27 @@ func ReadImages(path string) ([][]float64, error) {
     var rows int32
     var cols int32
     if err := binary.Read(f, binary.BigEndian, &magic); err != nil || magic != 2051 {
-        return nil, errors.New("mnistloader: cannot read magic number in images file")
+        return nil, 0, errors.New("mnistloader: cannot read magic number in images file")
     }
     if err := binary.Read(f, binary.BigEndian, &itemCount); err != nil {
-        return nil, errors.New("mnistloader: cannot read count of images")
+        return nil, 0, errors.New("mnistloader: cannot read count of images")
     }
     if err := binary.Read(f, binary.BigEndian, &rows); err != nil {
-        return nil, errors.New("mnistloader: cannot read number of rows of images")
+        return nil, 0, errors.New("mnistloader: cannot read number of rows of images")
     }
     if err := binary.Read(f, binary.BigEndian, &cols); err != nil {
-        return nil, errors.New("mnistloader: cannot read number of cols of images")
+        return nil, 0, errors.New("mnistloader: cannot read number of cols of images")
     }
     items := make([][]float64, itemCount)
     for i := int32(0); i < itemCount; i++ {
         item := make([]byte, rows*cols)
         items[i] = make([]float64, rows*cols)
         if err := binary.Read(f, binary.BigEndian, item); err != nil {
-            return nil, errors.New("mnistloader: cannot read image")
+            return nil, 0, errors.New("mnistloader: cannot read image")
         }
         for j, val := range item {
             items[i][j] = float64(val)
         }
     }
-    return items, nil
+    return items, int(rows*cols), nil
 }
